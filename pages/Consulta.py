31f81ecf222aee7fbc401d0b4baba012fab4f220
BM-great <67879045+BM-great@users.noreply.github.com>
2024-09-21 18:57:17 -0300

    Add files via upload

---
+++ pages/Consulta.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Consulta de Clientes",
+    page_icon="🔍"
+)
+
+dados = pd.read_csv("clientes.csv")
+
+st.title("Clientes cadastrados")
+
+st.divider()
+
+st.dataframe(dados)
